bddbdfd5b8a88cdd09075aff738fa4aba9ebad79
raulvivan <raul.vivanco@opendeusto.es>
2015-03-17 14:00:03 +0100

    Añadir tema de jsf con fallos

--- src/main/java/views/web/beans/AñadirTemaBean.java
+++ src/main/java/views/web/beans/AñadirTemaBean.java
@@ -1,13 +1,21 @@
 package views.web.beans;
 
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+
 import controllers.AñadirTemaController;
 import modelos.entities.Tema;
 
+@ManagedBean
 public class AñadirTemaBean extends ViewBean{
 	
 	private Tema tema;
 	
 	private AñadirTemaController añadirTemaController;
+	
+	public AñadirTemaBean(){
+		this.tema = new Tema();
+	}
 	
 	public Tema getTema() {
 		return tema;
@@ -20,7 +28,7 @@ public class AñadirTemaBean extends ViewBean{
 	public void añadirTema(){
 		añadirTemaController.añadirTema(tema);
 	}
-
+	@PostConstruct
 	public void process() {
 		añadirTemaController = this.getFactory().getAñadirTemaController();
 		this.añadirTema();
